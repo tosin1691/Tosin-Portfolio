d99bcc839854de8c274ce519ff1e7af7c9b10f82
tosin1691 <119135079+tosin1691@users.noreply.github.com>
2024-03-21 16:47:42 +0000

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Tosin-Portfolio/"
+  base: "/Tosin-Portfolio/",
   plugins: [react()],
 })
